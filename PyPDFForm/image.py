905a44451c085c0b733211d332cec555612e6fef
Emir Elçuk <emiirelcuk@gmail.com>
2024-12-11 09:02:04 +0300

    HVLSN-47 Add any_image_to_jpg Function

--- PyPDFForm/image.py
+++ PyPDFForm/image.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
-"""Contains helper for rotating images."""
+"""Contains helpers for image."""
 
 from io import BytesIO
 from typing import Union
 
 from PIL import Image
 
+
 def rotate_image(image_stream: bytes, rotation: Union[float, int]) -> bytes:
-    """Rotates an image by a specified rotation angle.
+    """Rotates an image by a rotation angle."""
 
-    Args:
-        image_stream (bytes): The input image as a byte stream.
-        rotation (Union[float, int]): The angle to rotate the image (in degrees).
-
-    Returns:
-        bytes: The rotated image as a byte stream.
-
-    Raises:
-        ValueError: If the input is not a valid image.
-    """
     buff = BytesIO()
     buff.write(image_stream)
     buff.seek(0)
@@ -34,4 +25,29 @@ def rotate_image(image_stream: bytes, rotation: Union[float, int]) -> bytes:
     buff.close()
     rotated_buff.close()
 
+    return result
+
+
+def any_image_to_jpg(image_stream: bytes) -> bytes:
+    """Converts an image of any type to jpg."""
+
+    buff = BytesIO()
+    buff.write(image_stream)
+    buff.seek(0)
+
+    image = Image.open(buff)
+
+    if image.format == "JPEG":
+        buff.close()
+        return image_stream
+
+    rgb_image = Image.new("RGB", image.size, (255, 255, 255))
+    rgb_image.paste(image, mask=image.split()[3] if len(image.split()) == 4 else None)
+
+    with BytesIO() as _file:
+        rgb_image.save(_file, format="JPEG")
+        _file.seek(0)
+        result = _file.read()
+
+    buff.close()
     return result
